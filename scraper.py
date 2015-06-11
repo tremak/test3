fa6e9d049db5d06ca2b7a8018bd12b194955a9a7
tremak <oikosnomos@hotmail.com>
2015-06-10 23:07:38 -0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1479,10 +1479,13 @@ try :
                             #print 'Horario :', horario
                             if len (re.findall(r'[Pp]art\s[Tt]ime', horario)) :
                                 hastaDia = 'Vi'
-                                if len (re.findall(r'\d+\W+\d+', horario)[0]) :
+                                if len (re.findall(r'\d+\W+\d+', horario)) :
                                 	inicio = re.findall(r'\d+\W+\d+', horario)[0]
-				if len (re.findall(r'\d+\W+\d+', horario)[1]) :
-					termino = re.findall(r'\d+\W+\d+', horario)[1]
+					if len (re.findall(r'\d+\W+\d+', horario))==2 :
+						termino = re.findall(r'\d+\W+\d+', horario)[1]
+				elif len (re.findall(r'\d+\W+\w+\W+\d+', horario)) :
+					inicio = re.findall(r'(\d+)\W+\w+\W+\d+', horario)[0] + ':00'
+					termino = re.findall(r'\d+\W+\w+\W+(\d+)', horario)[0] + ':00'
                             elif len (re.findall(r'[Ff]ull [Tt]ime', horario)) :
                                 hastaDia = 'Do'
                                 inicio = '00:00'
